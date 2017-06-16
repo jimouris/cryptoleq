5fec6d902a0f001b544c52a4b2bb918072573d6e
Dimitris Mouris <jimmouris@hotmail.com>
2017-06-16 15:45:56 +0300

    Minor changes

--- src/ceal/tests/tasks.c
+++ src/ceal/tests/tasks.c
@@ -10,155 +10,155 @@
 #include <unistd.h>
 
 char** parse(FILE* fp) {
-  size_t a_cap = 4;
-  char** a = malloc(a_cap * sizeof(char*));
-  size_t ai_cap = 16;
-  char* ai = malloc(ai_cap * sizeof(char));
-  size_t i = 0, j = 0;
-  int c;
-  while ((c = fgetc(fp)) != '\n' && c != EOF) {
-    if (isspace(c)) {
-      if (j == 0) continue;
-      ai[j] = 0;
-      a[i++] = ai;
-      if (i + 2 == a_cap) {
-        a_cap += a_cap;
-        a = realloc(a, a_cap * sizeof(char*));
-      }
-      ai_cap = 16;
-      ai = malloc(ai_cap * sizeof(char));
-      j = 0;
-    } else {
-      ai[j++] = c;
-      if (j == ai_cap) {
-        ai_cap += ai_cap;
-        ai = realloc(ai, ai_cap * sizeof(char));
-      }
+    size_t a_cap = 4;
+    char** a = malloc(a_cap * sizeof(char*));
+    size_t ai_cap = 16;
+    char* ai = malloc(ai_cap * sizeof(char));
+    size_t i = 0, j = 0;
+    int c;
+    while ((c = fgetc(fp)) != '\n' && c != EOF) {
+        if (isspace(c)) {
+            if (j == 0) continue;
+            ai[j] = 0;
+            a[i++] = ai;
+            if (i + 2 == a_cap) {
+                a_cap += a_cap;
+                a = realloc(a, a_cap * sizeof(char*));
+            }
+            ai_cap = 16;
+            ai = malloc(ai_cap * sizeof(char));
+            j = 0;
+        } else {
+            ai[j++] = c;
+            if (j == ai_cap) {
+                ai_cap += ai_cap;
+                ai = realloc(ai, ai_cap * sizeof(char));
+            }
+        }
     }
-  }
-  if (j) {
-    ai[j] = 0;
-    a[i++] = ai;
-  } else {
-    free(ai);
-  }
-  if (c == EOF && i == 0) {
-    free(a);
-    return NULL;
-  }
-  a[i] = NULL;
-  return a;
+    if (j) {
+        ai[j] = 0;
+        a[i++] = ai;
+    } else {
+        free(ai);
+    }
+    if (c == EOF && i == 0) {
+        free(a);
+        return NULL;
+    }
+    a[i] = NULL;
+    return a;
 }
 
 char** prepend(char** a, char* ai) {
-  size_t i = 0;
-  while (a[i] != NULL) ++i;
-  do {
-    a[i + 1] = a[i];
-  } while (i--);
-  a[0] = ai;
+    size_t i = 0;
+    while (a[i] != NULL) ++i;
+    do {
+        a[i + 1] = a[i];
+    } while (i--);
+    a[0] = ai;
 }
 
 int main(int argc, char* argv[]) {
-  if (argc != 3) {
-    fprintf(stderr, "Usage: %s [command file] [number of CPUs]\n", argv[0]);
-    return EXIT_FAILURE;
-  }
-  FILE* fp = fopen(argv[1], "r");
-  if (fp == NULL) {
-    fprintf(stderr, "%s: %s: File not found\n", argv[0], argv[1]);
-    return EXIT_FAILURE;
-  }
-  int cpus = atoi(argv[2]);
-  if (cpus <= 0 || cpus > 256) {
-    fprintf(stderr, "%s: %d: Invalid number of CPUs\n", argv[0], cpus);
+    if (argc != 3) {
+        fprintf(stderr, "Usage: %s [command file] [number of CPUs]\n", argv[0]);
+        return EXIT_FAILURE;
+    }
+    FILE* fp = fopen(argv[1], "r");
+    if (fp == NULL) {
+        fprintf(stderr, "%s: %s: File not found\n", argv[0], argv[1]);
+        return EXIT_FAILURE;
+    }
+    int cpus = atoi(argv[2]);
+    if (cpus <= 0 || cpus > 256) {
+        fprintf(stderr, "%s: %d: Invalid number of CPUs\n", argv[0], cpus);
+        fclose(fp);
+        return EXIT_FAILURE;
+    }
+    pid_t* pids = malloc(cpus * sizeof(pid_t));
+    int com = 0;
+    char** args;
+    while ((args = parse(fp)) != NULL) {
+        if (args[0] == NULL) continue;
+        int cpu = com++;
+        if (com > cpus) {
+            int status = EXIT_FAILURE;
+            pid_t pid = wait(&status);
+            if (pid == -1) {
+                fprintf(stderr, "%s: Failed to collect command\n", argv[0]);
+                return EXIT_FAILURE;
+            }
+            if (status != EXIT_SUCCESS) {
+                fprintf(stderr, "%s: Received failed status: %d\n", argv[0], status);
+                return EXIT_FAILURE;
+            }
+            cpu = 0;
+            while (pids[cpu] != pid) cpu += 1;
+            printf("Collected command on CPU %d\n", cpu);
+        }
+        pids[cpu] = fork();
+        if (pids[cpu] == (pid_t) -1) {
+            fprintf(stderr, "%s: Failed to create process for command %d\n", argv[0], com);
+            return EXIT_FAILURE;
+        }
+        if (pids[cpu] == 0) {
+            cpu_set_t* set = CPU_ALLOC(cpus);
+            size_t size = CPU_ALLOC_SIZE(cpus);
+            CPU_ZERO_S(size, set);
+            CPU_SET_S(cpu, size, set);
+            if (sched_setaffinity(getpid(), size, set) == -1) {
+                fprintf(stderr, "%s: Command %d failed to set affinity\n", argv[0], com);
+                return EXIT_FAILURE;
+            }
+            CPU_FREE(set);
+            char file[16];
+            sprintf(file, "out_%d.out", com);
+            int fd = open(file, O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
+            if (fd == -1 || dup2(fd, STDOUT_FILENO) == -1 || close(fd) == -1) {
+                fprintf(stderr, "%s: Command %d failed to set output file\n", argv[0], com);
+                return EXIT_FAILURE;
+            }
+            sprintf(file, "err_%d.out", com);
+            fd = open(file, O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
+            if (fd == -1 || dup2(fd, STDERR_FILENO) == -1 || close(fd) == -1) {
+                fprintf(stderr, "%s: Command %d failed to set error file\n", argv[0], com);
+                return EXIT_FAILURE;
+            }
+            prepend(args, "time");
+            execvp(args[0], args);
+            fprintf(stderr, "%s: Command %d failed to execute\n", argv[0], com);
+            return EXIT_FAILURE;
+        }
+        printf("Executing command %d: \"", com);
+        size_t i = 0;
+        while (args[i] != NULL) {
+            if (i) printf(" ");
+            printf("%s", args[i]);
+            free(args[i++]);
+        }
+        printf("\" on CPU %d\n", cpu);
+        fflush(stdout);
+        free(args);
+    }
     fclose(fp);
-    return EXIT_FAILURE;
-  }
-  pid_t* pids = malloc(cpus * sizeof(pid_t));
-  int com = 0;
-  char** args;
-  while ((args = parse(fp)) != NULL) {
-    if (args[0] == NULL) continue;
-    int cpu = com++;
-    if (com > cpus) {
-      int status = EXIT_FAILURE;
-      pid_t pid = wait(&status);
-      if (pid == -1) {
-        fprintf(stderr, "%s: Failed to collect command\n", argv[0]);
-        return EXIT_FAILURE;
-      }
-      if (status != EXIT_SUCCESS) {
-        fprintf(stderr, "%s: Received failed status: %d\n", argv[0], status);
-        return EXIT_FAILURE;
-      }
-      cpu = 0;
-      while (pids[cpu] != pid) cpu += 1;
-      printf("Collected command on CPU %d\n", cpu);
+    int i = 0;
+    while (i < com && i < cpus) {
+        int status = EXIT_FAILURE;
+        pid_t pid = wait(&status);
+        if (pid == -1) {
+            fprintf(stderr, "%s: Failed to collect command (final)\n", argv[0]);
+            return EXIT_FAILURE;
+        }
+        if (status != EXIT_SUCCESS) {
+            fprintf(stderr, "%s: Received failed status: %d\n", argv[0], status);
+            return EXIT_FAILURE;
+        }
+        int cpu = 0;
+        while (pids[cpu] != pid) cpu += 1;
+        printf("Collected command on CPU %d\n", cpu);
+        i += 1;
     }
-    pids[cpu] = fork();
-    if (pids[cpu] == (pid_t) -1) {
-      fprintf(stderr, "%s: Failed to create process for command %d\n", argv[0], com);
-      return EXIT_FAILURE;
-    }
-    if (pids[cpu] == 0) {
-      cpu_set_t* set = CPU_ALLOC(cpus);
-      size_t size = CPU_ALLOC_SIZE(cpus);
-      CPU_ZERO_S(size, set);
-      CPU_SET_S(cpu, size, set);
-      if (sched_setaffinity(getpid(), size, set) == -1) {
-        fprintf(stderr, "%s: Command %d failed to set affinity\n", argv[0], com);
-        return EXIT_FAILURE;
-      }
-      CPU_FREE(set);
-      char file[16];
-      sprintf(file, "out_%d.out", com);
-      int fd = open(file, O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
-      if (fd == -1 || dup2(fd, STDOUT_FILENO) == -1 || close(fd) == -1) {
-        fprintf(stderr, "%s: Command %d failed to set output file\n", argv[0], com);
-        return EXIT_FAILURE;
-      }
-      sprintf(file, "err_%d.out", com);
-      fd = open(file, O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
-      if (fd == -1 || dup2(fd, STDERR_FILENO) == -1 || close(fd) == -1) {
-        fprintf(stderr, "%s: Command %d failed to set error file\n", argv[0], com);
-        return EXIT_FAILURE;
-      }
-      prepend(args, "time");
-      execvp(args[0], args);
-      fprintf(stderr, "%s: Command %d failed to execute\n", argv[0], com);
-      return EXIT_FAILURE;
-    }
-    printf("Executing command %d: \"", com);
-    size_t i = 0;
-    while (args[i] != NULL) {
-      if (i) printf(" ");
-      printf("%s", args[i]);
-      free(args[i++]);
-    }
-    printf("\" on CPU %d\n", cpu);
-    fflush(stdout);
-    free(args);
-  }
-  fclose(fp);
-  int i = 0;
-  while (i < com && i < cpus) {
-    int status = EXIT_FAILURE;
-    pid_t pid = wait(&status);
-    if (pid == -1) {
-      fprintf(stderr, "%s: Failed to collect command (final)\n", argv[0]);
-      return EXIT_FAILURE;
-    }
-    if (status != EXIT_SUCCESS) {
-      fprintf(stderr, "%s: Received failed status: %d\n", argv[0], status);
-      return EXIT_FAILURE;
-    }
-    int cpu = 0;
-    while (pids[cpu] != pid) cpu += 1;
-    printf("Collected command on CPU %d\n", cpu);
-    i += 1;
-  }
-  printf("Executed %d commands successfully\n", com);
-  free(pids);
-  return EXIT_SUCCESS;
+    printf("Executed %d commands successfully\n", com);
+    free(pids);
+    return EXIT_SUCCESS;
 }
