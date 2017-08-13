d9078b0a2f04986a554fa1c78cd338cfd8084cc2
Dimitris Mouris <jimouris@gmail.com>
2017-08-13 17:54:45 +0300

    Add permutations graph

--- src/ceal/tests/gen2_plots.py
+++ src/ceal/tests/gen2_plots.py
@@ -6,12 +6,12 @@ import sys
 import string
 
 if len(sys.argv) != 2:
-    print "Usage:\t python " + sys.argv[0] + " [primes|tak|isort|psi|mmult]"
+    print "Usage:\t python " + sys.argv[0] + " [primes|tak|isort|psi|mmult|perm]"
     sys.exit(1)
-if sys.argv[1] == "primes" or sys.argv[1] == "tak" or sys.argv[1] == "isort" or sys.argv[1] == "psi" or sys.argv[1] == "mmult":
+if sys.argv[1] == "primes" or sys.argv[1] == "tak" or sys.argv[1] == "isort" or sys.argv[1] == "psi" or sys.argv[1] == "mmult" or sys.argv[1] == "perm":
     benchmark = sys.argv[1]
 else:
-    print "Usage:\t python " + sys.argv[0] + " [primes|tak|isort|psi]"
+    print "Usage:\t python " + sys.argv[0] + " [primes|tak|isort|psi|mmult|perm]"
     sys.exit(1)
 
 mydpi = 300
@@ -27,8 +27,8 @@ elif benchmark == "psi":
     pltsize = (6, 2.2) # default (8, 6)
 elif benchmark == "mmult":
     pltsize = (6, 2.2) # default (8, 6)
-elif benchmark == "permutations":
-    pltsize = (6, 2.2) # default (8, 6)
+elif benchmark == "perm":
+    pltsize = (6.7, 2.2) # default (8, 6)
 
 nbits = ['64-bit $\lambda$', '128-bit $\lambda$', '256-bit $\lambda$', '512-bit $\lambda$', '1024-bit $\lambda$']
 
@@ -38,19 +38,15 @@ data = {
     'primes' :  { '64' : [80.57, 157.46, 317.46], '128' : [190.43, 366.87, 729.19], '256' : [433.87, 816.97, 1598.71], '512' : [1397.44, 2468.49, 4618.86],  '1024' : [4388.98, 7789.04, 14060.56] },
     'isort' :  { '64' : [152.93, 615.65, 2511.54], '128' : [349.79, 1420.64, 5707.16], '256' : [757.48, 3084.15, 12375.45], '512' : [2171.9, 8734.65, 35494.2],  '1024' : [6777.22, 27367.96, 108343.76] },
     'psi' :  { '64' : [249.25, 968.8, 3845.82], '128' : [379.71, 1462.32, 5786.62], '256' : [828.24, 3127.55, 12225.06], '512' : [2479.54, 9191.15, 36009.76],  '1024' : [7133.17, 27015.24, 108343.76] },
-
-    'mmult' :  { '64' : [45.22, 125.24, 216.41], '128' : [106.13, 322.82, 568.54], '256' : [255.38, 642.45, 1540.36], '512' : [700.67, 2962.81, 5138.3],  '1024' : [2267.09, 11030.12, 19218.85] }
-
-    # 'permutations' :  { '64' : [249.25, 968.8, 3845.82], '128' : [379.71, 1462.32, 5786.62], '256' : [828.24, 3127.55, 12225.06], '512' : [2479.54, 9191.15, 36009.76],  '1024' : [7133.17, 27015.24, 108343.76] }
+    'mmult' :  { '64' : [45.22, 125.24, 216.41], '128' : [106.13, 322.82, 568.54], '256' : [255.38, 642.45, 1540.36], '512' : [700.67, 2962.81, 5138.3],  '1024' : [2267.09, 11030.12, 19218.85] },
+    'perm' :  { '64' : [0.24, 0.27, 0.4, 1.05], '128' : [0.6, 0.62, 0.71, 1.44], '256' : [1.14, 1.19, 1.55, 2.17], '512' : [3.44, 3.7, 3.87, 4.83],  '1024' : [11.97, 12.16, 12.86, 14.02] }
 }
 
 for key, value in data[benchmark].items():
     for i in range(len(value)):
         if benchmark == "tak" or benchmark == "isort" or benchmark == "psi":
             value[i] /= 10000
-        elif benchmark == "primes":
-            value[i] /= 1000
-        elif benchmark == "mmult":
+        elif benchmark == "primes" or benchmark == "mmult":
             value[i] /= 1000
     data[benchmark][key] = value
 
@@ -60,11 +56,9 @@ x_axis_labels = {
     'primes' : ("Maximum number to compute", [256, 512, 1024]),
     'isort' : ("Number of Array Elements ($N$)", [32, 64, 128]),
     'psi' : ("Number of Each Array Elements ($N \cap N$)", ["$16 \cap 16$", "$32 \cap 32$", "$64 \cap 64$"]),
-    
-    'mmult' : ("Matrices Size", ['8x8 x 8x8', '12x8 x 8x12', '16x8 x 8x16'])
+    'mmult' : ("Matrices Size", ['$[8 \\times 8] \\times [8 \\times 8]$', '$[12 \\times 8] \\times [8 \\times 12]$', '$[16 \\times 8] \\times [8 \\times 16]$']),
+    'perm' : ("Items", [3, 4, 5, 6])
 }
-
-# max_num = [256, 512, 1024]
 
 data64 = data[benchmark]['64']
 data128 = data[benchmark]['128']
@@ -79,7 +73,7 @@ width = 0.185       # the width of the bars
 
 fig, ax = plt.subplots(figsize=pltsize)
 
-ax.margins(0.03, 0.03)  
+ax.margins(0.03, 0.03)
 
 rects1 = ax.bar(index - 2*width, data64, width, color='xkcd:light pink', hatch='xxx', edgecolor='black', linewidth=1)
 rects2 = ax.bar(index - width, data128, width, color='xkcd:very light blue', hatch='...', edgecolor='black', linewidth=1)
@@ -113,10 +107,16 @@ elif benchmark == "psi":
 elif benchmark == "mmult":
     ax.set_ylim([0.04, 100])
     ax.set_ylabel("time (sec.) x $10^3$")
+elif benchmark == "perm":
+    ax.set_ylim([0.1, 100])
+    ax.set_ylabel("time (sec.)")
 ax.set_xticks(index)
 ax.set_xlabel(x_axis_labels[benchmark][0])
 ax.set_xticklabels(x_axis_labels[benchmark][1])
-ax.legend((rects1[0], rects2[0], rects3[0], rects4[0], rects5[0]), nbits, fontsize=8)
+if benchmark == "perm":
+    ax.legend((rects1[0], rects2[0], rects3[0], rects4[0], rects5[0]), nbits, fontsize=8, ncol=5)
+else:
+    ax.legend((rects1[0], rects2[0], rects3[0], rects4[0], rects5[0]), nbits, fontsize=8)
 
 def autolabel(rects):
     for rect in rects:
