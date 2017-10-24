877755c825d70d7e7b196ad1c75d4289bd71329f
Dimitris Mouris <jimouris@gmail.com>
2017-10-24 15:30:57 +0300

    Add simon low level with ROR instead of ROL

--- src/ceal/tests/SimonCipher/simon.c
+++ src/ceal/tests/SimonCipher/simon.c
@@ -4,20 +4,7 @@
 #include <stdint.h>
 #include <inttypes.h>
 
-#define WORD_SIZE 16
-#define KEY_WORDS 4
 #define ROUNDS 32
-
-#define WORD_MASK ((0x1ull << (WORD_SIZE&63)) - 1)
-#define C ((0xffffffffffffffffull ^ 0x3ull) & WORD_MASK)
-
-uint16_t z[5][62] = {
-    {1,1,1,1,1,0,1,0,0,0,1,0,0,1,0,1,0,1,1,0,0,0,0,1,1,1,0,0,1,1,0,1,1,1,1,1,0,1,0,0,0,1,0,0,1,0,1,0,1,1,0,0,0,0,1,1,1,0,0,1,1,0},
-    {1,0,0,0,1,1,1,0,1,1,1,1,1,0,0,1,0,0,1,1,0,0,0,0,1,0,1,1,0,1,0,1,0,0,0,1,1,1,0,1,1,1,1,1,0,0,1,0,0,1,1,0,0,0,0,1,0,1,1,0,1,0},
-    {1,0,1,0,1,1,1,1,0,1,1,1,0,0,0,0,0,0,1,1,0,1,0,0,1,0,0,1,1,0,0,0,1,0,1,0,0,0,0,1,0,0,0,1,1,1,1,1,1,0,0,1,0,1,1,0,1,1,0,0,1,1},
-    {1,1,0,1,1,0,1,1,1,0,1,0,1,1,0,0,0,1,1,0,0,1,0,1,1,1,1,0,0,0,0,0,0,1,0,0,1,0,0,0,1,0,1,0,0,1,1,1,0,0,1,1,0,1,0,0,0,0,1,1,1,1},
-    {1,1,0,1,0,0,0,1,1,1,1,0,0,1,1,0,1,0,1,1,0,1,1,0,0,0,1,0,0,0,0,0,0,1,0,1,1,1,0,0,0,0,1,1,0,0,1,0,1,0,0,1,0,0,1,1,1,0,1,1,1,1}
-};
 
 #define ROR(x, r) ((x >> r) | (x << ((sizeof(uint16_t) * 8) - r)))
 #define ROL(x, r) ((x << r) | (x >> ((sizeof(uint16_t) * 8) - r)))
@@ -30,6 +17,23 @@ void simon_encrypt(uint16_t const pt[static 2], uint16_t ct[static 2], uint16_t 
         tmp = ct[0];
         ct[0] = ct[1] ^ (ROL(ct[0], 1) & ROL(ct[0], 8)) ^ ROL(ct[0], 2) ^ K[i];
         ct[1] = tmp;
+    }
+}
+
+// Simon using ROR instead of ROL
+void simon_encrypt_ll(uint16_t const pt[static 2], uint16_t ct[static 2], uint16_t const K[static ROUNDS]) {
+    ct[0] = pt[0];
+    ct[1] = pt[1];
+    uint16_t ct0_tmp, tmp_1, tmp_3;
+    for (int i = 0; i < ROUNDS; ++i) {
+        ct0_tmp = ct[0];
+        tmp_1 = ROR(ct[0], 15);
+        tmp_3 = ROR(ct[0], 8);
+        tmp_1 &= tmp_3;
+        tmp_1 ^= ct[1];
+        tmp_1 ^= ROR(ct[0], 14);
+        ct[0] = tmp_1 ^ K[i];
+        ct[1] = ct0_tmp;
     }
 }
 
@@ -52,7 +56,7 @@ int main(void) {
     uint16_t exp[ROUNDS] = { 256, 2312, 4368, 6424, 29123, 46665, 22228, 57456, 61786, 50485, 56724, 16400, 9482, 28518, 59755, 19416, 4069, 31815, 57583, 15905, 1627, 17292, 62058, 46528, 34313, 40846, 55487, 2476, 59410, 10000, 11434, 36116};
 
     printf("PTL: %" PRIu16 ", PTR: %" PRIu16 " \n", plain_text[0], plain_text[1]);
-    simon_encrypt(plain_text, buffer, exp);
+    simon_encrypt_ll(plain_text, buffer, exp);
     printf("CTL: %" PRIu16 ", CTR: %" PRIu16 " \n", buffer[0], buffer[1]);
 
     if (buffer[0] != cipher_text[0] || buffer[1] != cipher_text[1]) {
