746987ee9f943282793dba2695d14e05c9abc660
Dimitris Mouris <jimouris@gmail.com>
2017-10-31 10:47:04 +0200

    Fix jenkins for 16bit blocks Input&Output

--- src/ceal/tests/jenkins/jenkins.c
+++ src/ceal/tests/jenkins/jenkins.c
@@ -1,23 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <inttypes.h>
 #include <assert.h>
 
-uint32_t jenkins_one_at_a_time_hash(const uint8_t* key, size_t length) {
-    size_t i = 0;
-    uint32_t hash = 0;
-    while (i != length) {
-        hash += key[i++];
-        hash += hash << 10;
-        hash ^= hash >> 6;
-    }
-    hash += hash << 3;
-    hash ^= hash >> 11;
-    hash += hash << 15;
-    return hash;
-}
-
-uint32_t jenkins_ll(const uint8_t* key, size_t length) {
+uint32_t jenkins_16b_ll(const uint16_t* key, size_t length) {
     size_t i = 0;
     uint32_t hash = 0;
     uint32_t temp;
@@ -26,30 +13,50 @@ uint32_t jenkins_ll(const uint8_t* key, size_t length) {
         i++;
         hash += temp;
         temp = hash << 10;
+        temp &= 0xffff;
         hash += temp;
         temp = hash >> 6;
-        printf("%zu\n", hash^temp);
         hash ^= temp;
+        printf(" %" PRIu32 "\n", hash);
     } while (i != length);
     temp = hash << 3;
     hash += temp;
     temp = hash >> 11;
     hash ^= temp;
+    printf("after xor %" PRIu32 " %" PRIu32 "\n", hash, temp);
     temp = hash << 15;
+    printf(" %" PRIu32 "\n", temp);
+    temp &= 0xffff;
+    printf(" %" PRIu32 "\n", temp);
     hash += temp;
+    printf(" %" PRIu32 "\n", hash);
+    hash &= 0xffff;
     return hash;
 }
 
 int main(void) {
-    size_t length = 4;
-    uint8_t* array = malloc(length * sizeof(uint8_t));
-    for (int i = 0 ; i < length ; i++) {
-        array[i] = i + 1;
-    }
-
-    // assert(jenkins_one_at_a_time_hash(array, length) == jenkins_ll(array, length));
-    printf("hash : %zu\n", jenkins_ll(array, length));
-
+    size_t length = 2;
+    uint16_t* array = malloc(length * sizeof(uint16_t));
+    array[0] = 24930; // ab
+    array[1] = 25444; // cd
+    printf("hash : %" PRIu32 "\n", jenkins_16b_ll(array, length));
     return 0;
 }
 
+// uint32_t jenkins_one_at_a_time_hash(const uint8_t* key, size_t length) {
+//     size_t i = 0;
+//     uint32_t hash = 0;
+//     while (i != length) {
+//         hash += key[i++];
+//         hash += hash << 10;
+//         hash ^= hash >> 6;
+//     }
+//     hash += hash << 3;
+//     hash ^= hash >> 11;
+//     hash += hash << 15;
+//     return hash;
+// }
+
+// 000011110100111100110000000000000000
+// 100011110100111100110000000000000000
+// 11110100111100110000000000000000
