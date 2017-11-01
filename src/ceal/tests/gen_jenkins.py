1cded791447278a1ccf0beba8dca82ac7a70c7e4
Dimitris Mouris <jimouris@gmail.com>
2017-11-01 15:54:05 +0200

    Add open jenkins timings

--- src/ceal/tests/gen_jenkins.py
+++ src/ceal/tests/gen_jenkins.py
@@ -15,8 +15,8 @@ pltsize = (5.5, 2) # default (8, 6)
 nbits = [64, 128, 256, 512, 1024]
 
 data = {
-    'jenkins':    { 'jenkins' : [892.69, 987.97, 2689.28, 6668.12, 18316.79] }
-    # 'jenkinsopn': { 'jenkins' : [36.15, 40.65, 45.81, 56.41, 81.38] }
+    'jenkins':    { 'jenkins' : [892.69, 987.97, 2689.28, 6668.12, 18316.79] },
+    'jenkinsopn': { 'jenkins' : [18.97, 22.45, 25.21, 31.81, 47.45] }
 }
 
 for key, value in data[benchmark].items():
@@ -24,13 +24,13 @@ for key, value in data[benchmark].items():
         value[i] /= 1000
     data[benchmark][key] = value
 
-# for key, value in data[benchmark+"opn"].items():
-#     for i in range(len(value)):
-#         value[i] /= 1000
-#     data[benchmark+"opn"][key] = value
+for key, value in data[benchmark+"opn"].items():
+    for i in range(len(value)):
+        value[i] /= 1000
+    data[benchmark+"opn"][key] = value
 
 jenkins = data[benchmark]['jenkins']
-# jenkinsopn = data[benchmark+"opn"]['jenkins']
+jenkinsopn = data[benchmark+"opn"]['jenkins']
 
 N = len(jenkins)
 index = np.arange(N)  # the x locations for the groups
@@ -39,11 +39,11 @@ width = 0.6       # the width of the bars
 fig, ax = plt.subplots(figsize=pltsize)
 ax.margins(0.04, 0.04) 
 rects1 = ax.bar(index+width/2, jenkins, width, color='xkcd:light pink', hatch='xxx', edgecolor='black', linewidth=1)
+rects1opn = ax.bar(index+width/2, jenkinsopn, width, color='xkcd:light pink', edgecolor='black', linewidth=1)
 
-# rects1opn = ax.bar(index, jenkinsopn, width, color='xkcd:light pink', edgecolor='black', linewidth=1)
 
 ax.set_yscale('log')
-ax.set_ylim([0.01, 1000])
+ax.set_ylim([0.001, 100])
 ax.set_ylabel("time (sec.) x $10^3$")
 ax.set_xlabel("Security Parameter ($\lambda$) bits")
 ax.set_xticks(index + width / 2)
